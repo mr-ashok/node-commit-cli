43cfa08820c548d2d460adc80fef69a82a70607d
Ashok <ashok@deldsim.com>
2021-04-27 01:41:55 +0530

    fix(cli): fix cli library link issue
    - Currently, the cli library cannot be used inside other projects.
    - Made necessary changes to execute cli in other projects.

--- dist/index.js
+++ dist/index.js
@@ -1,45 +1,39 @@
 #!/usr/bin/env node
-namespace Index {
+var Index;
+(function (Index) {
     const commitPrompt = require('./CommitPrompt');
     const figlet = require('figlet');
     const chalk = require('chalk');
     const fs = require('fs');
     const constants = require('./constants');
     const { exec } = require("child_process");
-
-    fs.readFile('./commit-config.json', 'utf8', (err: any, data:string) => {
+    fs.readFile('./commit-config.json', 'utf8', (err, data) => {
         if (err) {
             throw err;
         }
         const config = JSON.parse(data);
-        const projectName: string = config['projectName'];
-        const commitDescriptionLength: number = config['commitDescriptionLength'] || 84;
+        const projectName = config['projectName'];
+        const commitDescriptionLength = config['commitDescriptionLength'] || 84;
         const rawComponents = config['components'];
-        const components: string[] = Array.isArray(rawComponents) ? rawComponents : [];
-
+        const components = Array.isArray(rawComponents) ? rawComponents : [];
         const issueTracker = config['issueTracker'] || {};
-        const issueTrackerName: string = issueTracker['name'];
-        const issueTrackerIdType: string = issueTracker['idType'] || 'id';
-
-        console.log(chalk.hex('#1a598c')(figlet.textSync(projectName, {horizontalLayout: 'full'})));
-
-        commitPrompt.askQuestion(commitDescriptionLength, components, issueTrackerName, issueTrackerIdType).then((answers: any) => {
-            const rawCommitType: string = answers[constants.QUESTION_KEY_COMMIT_TYPE] || '';
+        const issueTrackerName = issueTracker['name'];
+        const issueTrackerIdType = issueTracker['idType'] || 'id';
+        console.log(chalk.hex('#1a598c')(figlet.textSync(projectName, { horizontalLayout: 'full' })));
+        commitPrompt.askQuestion(commitDescriptionLength, components, issueTrackerName, issueTrackerIdType).then((answers) => {
+            const rawCommitType = answers[constants.QUESTION_KEY_COMMIT_TYPE] || '';
             const commitType = rawCommitType.substring(0, rawCommitType.indexOf(':'));
-
-            const commitComponent: string = answers[constants.QUESTION_KEY_COMPONENT];
-            const commitDescription: string = answers[constants.QUESTION_KEY_DESCRIPTION];
-            const commitDetails: string = answers[constants.QUESTION_KEY_COMMIT_MESSAGE];
-
+            const commitComponent = answers[constants.QUESTION_KEY_COMPONENT];
+            const commitDescription = answers[constants.QUESTION_KEY_DESCRIPTION];
+            const commitDetails = answers[constants.QUESTION_KEY_COMMIT_MESSAGE];
             const description = `${commitType}(${commitComponent}): ${commitDescription}`.toLowerCase();
             let commitMessage = `${description}\n\n${commitDetails}`;
             if (issueTrackerName) {
-                const issueId: string =  answers[constants.QUESTION_KEY_ISSUE_TRACKER_ID];
+                const issueId = answers[constants.QUESTION_KEY_ISSUE_TRACKER_ID];
                 commitMessage += `\n${issueTrackerName}: ${issueId}`;
             }
-
             const command = `git commit -m '${commitMessage}'`;
-            exec(command, (error: { message: any; }, stdout: any, stderr: any) => {
+            exec(command, (error, stdout, stderr) => {
                 if (error) {
                     console.log(`error: ${error.message}`);
                     return;
@@ -50,8 +44,7 @@ namespace Index {
                 }
                 console.log(`stdout: ${stdout}`);
             });
-
         });
     });
-
-}
+})(Index || (Index = {}));
+//# sourceMappingURL=index.js.map
